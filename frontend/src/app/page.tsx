5e509e9702cad258ad4ee356b134058807cde056
Xnths <jonathascastilhoes@gmail.com>
2025-07-08 13:53:35 -0300

    feat: adding cards

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,22 +3,40 @@ import { Book } from "../../types/books";
 
 import {
   Card,
-  CardAction,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function Home() {
   const books: Book[] = await api.get("/api/v1/books");
 
   return (
-    <div>
-      {books.map((book) => (
-        <Card key={book.id}>{book.title}</Card>
-      ))}
+    <div className="w-screen h-full p-8">
+      <div className="flex justify-center items-center w-full">
+        <div className="flex flex-row flex-wrap items-center gap-4">
+          {books.map((book) => (
+            <Card
+              key={book.id}
+              className="w-[300px] h-[200px] flex flex-col justify-between"
+            >
+              <CardHeader>
+                <CardTitle>{book.title}</CardTitle>
+                <CardDescription>{book.author}</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <Link href={"/"}>
+                  <Button variant="outline">Detalhes</Button>
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
