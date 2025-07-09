0b23b97916c82dbdb197880a935e126b2310e39e
Xnths <jonathascastilhoes@gmail.com>
2025-07-09 01:18:18 -0300

    feat: adding tanstack to the project

--- frontend/lib/http/get-books.ts
+++ frontend/lib/http/get-books.ts
@@ -6,6 +6,7 @@ import { api } from "../api";
 interface GetBooksRequest {
   page: number;
   pageSize: number;
+  q?: string;
 }
 
 interface GetBooksResponse {
@@ -13,12 +14,10 @@ interface GetBooksResponse {
   total: number;
 }
 
-export const getBooks = async ({ page, pageSize }: GetBooksRequest) => {
+export const getBooks = async ({ page, pageSize, q = "" }: GetBooksRequest) => {
   const data = await api.get<GetBooksResponse>(
-    `/api/v1/books?page=${page}&page_size=${pageSize}`
+    `/api/v1/books?q=${q}&page=${page}&page_size=${pageSize}`
   );
-
-  console.log(data);
 
   return data;
 };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardDescription,
@@ -12,45 +14,53 @@ import Link from "next/link";
 import { bookPage } from "../../lib/routes";
 import { getBooks } from "../../lib/http/get-books";
 import { Label } from "@/components/ui/label";
+import { useQuery } from "@tanstack/react-query";
 
-export default async function Home() {
+export default function Home() {
   const page = 1;
   const pageSize = 9;
-  const { books } = await getBooks({ page, pageSize });
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["books", page, pageSize],
+    queryFn: () => getBooks({ page, pageSize }),
+  });
+
+  if (isLoading) return <div>Carregando...</div>;
+  if (error) return <div>Erro ao carregar livros</div>;
 
   return (
     <div className="w-screen h-full p-8">
       <div className="flex flex-col justify-center w-full gap-8">
         <Button className="w-fit">Adicionar livro</Button>
-        <form>
+        <form method="GET" action="">
           <div>
             <Label htmlFor="search">Buscar:</Label>
             <Input
               id="search"
+              name="q"
               type="text"
               placeholder="Título do livro ou autor"
             />
           </div>
-          <Button>Buscar</Button>
+          <Button type="submit">Buscar</Button>
         </form>
         <div className="flex flex-row flex-wrap items-center gap-4">
-          {books &&
-            books.map((book) => (
-              <Card
-                key={book.id}
-                className="w-[300px] h-[200px] flex flex-col justify-between"
-              >
-                <CardHeader>
-                  <CardTitle>{book.title}</CardTitle>
-                  <CardDescription>{book.author}</CardDescription>
-                </CardHeader>
-                <CardFooter>
-                  <Link href={bookPage(book.id)}>
-                    <Button variant="outline">Detalhes</Button>
-                  </Link>
-                </CardFooter>
-              </Card>
-            ))}
+          {data?.books?.map((book) => (
+            <Card
+              key={book.id}
+              className="w-[300px] h-[200px] flex flex-col justify-between"
+            >
+              <CardHeader>
+                <CardTitle>{book.title}</CardTitle>
+                <CardDescription>{book.author}</CardDescription>
+              </CardHeader>
+              <CardFooter>
+                <Link href={bookPage(book.id)}>
+                  <Button variant="outline">Detalhes</Button>
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
       </div>
     </div>
